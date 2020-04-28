cf381248b024004b76619e7a59735daade788153
George55555 <ggborushko@yahoo.com>
2020-04-28 19:30:35 -0400

    added CLIENT_VERSION_SUFFIX

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Sync");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   ""
+#define CLIENT_VERSION_SUFFIX   "Sync"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
